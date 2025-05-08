91db7f401e8faeb179a553403cbc01c164de392e
jhonathan55 <jfgutierrezsaico@gmail.com>
2025-05-07 21:53:28 -0400

    firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'front-payments';
+  title = 'IP Chile';
 }
